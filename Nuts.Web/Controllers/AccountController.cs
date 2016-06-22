c8021e688ecffef82b98d9e312d5f5fe391f2870
nakaji <nakaji126@gmail.com>
2016-06-23 07:56:01 +0900

    Twitter認証を追加

---
+++ Nuts.Web/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using CoreTweet;
+
+namespace Nuts.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+        public async Task<ActionResult> Login()
+        {
+            var domain = Request.Url.GetComponents(UriComponents.SchemeAndServer, UriFormat.UriEscaped);
+            var oAuthSession = await OAuth.AuthorizeAsync(
+                ConfigurationManager.AppSettings["consumerKey"],
+                ConfigurationManager.AppSettings["consumerSecret"],
+                $"{domain}/Account/Callback");
+            TempData["OAuthSession"] = oAuthSession;
+
+            return Redirect(oAuthSession.AuthorizeUri.OriginalString);
+        }
+
+        public async Task<ActionResult> Callback(string oauth_token, string oauth_verifier)
+        {
+            var oAuthSession = TempData["OAuthSession"] as OAuth.OAuthSession;
+            var token = await oAuthSession.GetTokensAsync(oauth_verifier);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
