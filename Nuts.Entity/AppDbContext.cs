541c76c4856358fb99cd6d52673ee67cd3ff554c
nakaji <nakaji126@gmail.com>
2016-06-24 04:20:20 +0900

    Entityプロジェクトの追加

---
+++ Nuts.Entity/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuts.Entity
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext() : base("NutsConnection") { }
+    }
+}
