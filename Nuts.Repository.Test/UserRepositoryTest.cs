f3c097b4269c82ccdbfee2e0630ea2198406a80d
nakaji <nakaji126@gmail.com>
2016-06-28 05:19:06 +0900

    最初のテストを追加

---
+++ Nuts.Repository.Test/UserRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nuts.Repository.Test
+{
+    [TestClass]
+    public class UserRepositoryTest
+    {
+        [TestMethod]
+        public void GetUserByTwitterUserId_存在しない場合はnull()
+        {
+            // Arrange
+            var sut = new UserRepository();
+
+            // Act
+            var result = sut.GetUserByTwitterUserId(100);
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+    }
+}
---
+++ Nuts.Repository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Nuts.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuts.Repository
+{
+    public class UserRepository
+    {
+        public User GetUserByTwitterUserId(long userId)
+        {
+            return null;
+        }
+    }
+}
