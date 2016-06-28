53f6f58f205b5ffbffefc62cb40085e7792903cb
nakaji <nakaji126@gmail.com>
2016-06-29 04:34:52 +0900

    リファクタリング

--- Nuts.Repository.Test/UserRepositoryTest.cs
+++ Nuts.Repository.Test/UserRepositoryTest.cs
@@ -11,23 +11,11 @@ namespace Nuts.Repository.Test
     [TestClass]
     public class UserRepositoryTest
     {
-        [TestMethod]
-        public void GetUserByTwitterUserId_存在しない場合はnull()
+        private Mock<AppDbContext> _moqDb;
+
+        [TestInitialize]
+        public void SetUp()
         {
-            // Arrange
-            var sut = new UserRepository();
-
-            // Act
-            var result = sut.GetUserByTwitterUserId(100);
-
-            // Assert
-            Assert.AreEqual(null, result);
-        }
-
-        [TestMethod]
-        public void GetUserByTwitterUserId_UserIdで検索()
-        {
-            // Arrange
             var data = new List<User>()   {
                 new User { Id = 1, Twitter=new Twitter() {UserId=100} },
                 new User { Id = 2, Twitter=new Twitter() {UserId=200} },
@@ -39,10 +27,28 @@ namespace Nuts.Repository.Test
             dbSetMock.Setup(m => m.ElementType).Returns(data.ElementType);
             dbSetMock.Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
 
-            var moqDb = new Mock<AppDbContext>();
-            moqDb.Setup(x => x.Users).Returns(dbSetMock.Object);
+            _moqDb = new Mock<AppDbContext>();
+            _moqDb.Setup(x => x.Users).Returns(dbSetMock.Object);
+        }
 
-            var sut = new UserRepository(moqDb.Object);
+        [TestMethod]
+        public void GetUserByTwitterUserId_存在しない場合はnull()
+        {
+            // Arrange
+            var sut = new UserRepository(_moqDb.Object);
+
+            // Act
+            var result = sut.GetUserByTwitterUserId(9999);
+
+            // Assert
+            Assert.AreEqual(null, result);
+        }
+
+        [TestMethod]
+        public void GetUserByTwitterUserId_UserIdで検索()
+        {
+            // Arrange
+            var sut = new UserRepository(_moqDb.Object);
 
             // Act
             var result = sut.GetUserByTwitterUserId(100);
@@ -50,7 +56,6 @@ namespace Nuts.Repository.Test
             // Assert
             Assert.AreEqual(1, result.Id);
             Assert.AreEqual(1, result.Id);
-
         }
     }
 }
