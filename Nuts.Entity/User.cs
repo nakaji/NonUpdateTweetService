fa28e4aea64e5fc39ee5feff12068c2e256ecb39
nakaji <nakaji126@gmail.com>
2016-06-30 05:35:06 +0900

    Mod:TwitterのUserIdを一意にする

--- Nuts.Entity/User.cs
+++ Nuts.Entity/User.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Nuts.Entity
 {
@@ -14,6 +16,7 @@ namespace Nuts.Entity
 
     public class Twitter
     {
+        [Index(IsUnique =true)]
         public long UserId { get; set; }
         public string ScreenName { get; set; }
         public string AccessToken { get; set; }
