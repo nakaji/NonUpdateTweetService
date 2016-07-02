34569696497d48d84a70edec3acefe0574ef48a6
nakaji <nakaji126@gmail.com>
2016-07-03 05:49:04 +0900

    Add:RssReader

---
+++ Nuts.Service.Test/RssReaderTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nuts.Service.Test
+{
+    [TestClass]
+    public class RssReaderTest
+    {
+        [TestMethod]
+        public void URLからRSSを取得する()
+        {
+            // Arrange
+            var reader = new RssReader();
+            var url = "http://blog.nakajix.jp/feed";
+
+            // Act
+            SyndicationFeed result = reader.GetFeed(url);
+
+            // Assert
+            Assert.AreEqual("なか日記", result.Title.Text);
+        }
+    }
+}
---
+++ Nuts.Service/RssReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Nuts.Service
+{
+    public class RssReader
+    {
+        public SyndicationFeed GetFeed(string url)
+        {
+            using (var reader = XmlReader.Create(url))
+            {
+                return SyndicationFeed.Load(reader);
+            }
+        }
+    }
+}
