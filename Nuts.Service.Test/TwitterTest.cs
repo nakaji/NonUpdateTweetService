60bc6d4f7f26bfef586e64d88ec0a7ce85eb5d61
nakaji <nakaji126@gmail.com>
2016-07-07 03:58:39 +0900

    Add:Project

---
+++ Nuts.Service.Test/TwitterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nuts.Service;
+
+namespace Nuts.Service.Test
+{
+    [TestClass]
+    public class TwitterTest
+    {
+        [TestMethod]
+        public void Status_ツイートする()
+        {
+            // Arrange
+            var sut = new Twitter();
+
+            // Act
+            
+
+            // Assert
+
+        }
+    }
+}
