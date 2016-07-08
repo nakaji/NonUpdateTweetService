21746a824f75185c7970b5c36f69109a06bae5bf
nakaji <nakaji126@gmail.com>
2016-07-09 04:03:48 +0900

    Add:Index用のViewModelを追加

---
+++ Nuts.Web/ViewModels/SettingsViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nuts.Entity;
+
+namespace Nuts.Web.ViewModels
+{
+    public class SettingsIndexViewModel
+    {
+        public string UserId { get; set; }
+        public string ScreetName { get; set; }
+
+        public List<Setting> Settings { get; set; }
+    }
+}
