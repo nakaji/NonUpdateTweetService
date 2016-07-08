9875afaa26d625c91b6424aa0ee84b072fc916b4
nakaji <nakaji126@gmail.com>
2016-07-09 04:05:46 +0900

    Add:テストプロジェクトを追加

---
+++ Nuts.Web.Test/WorkerService/SettingServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nuts.Web.ViewModels;
+using Nuts.Web.WorkerService;
+
+namespace Nuts.Web.Test.WorkerService
+{
+    [TestClass]
+    public class SettingServiceTest
+    {
+        [TestMethod]
+        public void GetSettingsIndexViewModel_UserIdから取得する()
+        {
+            // Arrange
+            var sut = new SettingService();
+
+            // Act
+            var restult = sut.GetSettingsIndexViewModel(100);
+
+            // Assert
+            Assert.IsNotNull(restult);
+        }
+    }
+}
