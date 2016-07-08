1e033b1ff88cad529848de15fc1ab6d206e5c7ba
nakaji <nakaji126@gmail.com>
2016-07-09 05:27:50 +0900

    一覧画面の雛形を作成

--- Nuts.Web/Controllers/SettingsController.cs
+++ Nuts.Web/Controllers/SettingsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Nuts.Web.WorkerService;
 
 namespace Nuts.Web.Controllers
 {
@@ -11,7 +12,9 @@ namespace Nuts.Web.Controllers
         // GET: Settings
         public ActionResult Index()
         {
-            return View();
+            var service = new SettingService();
+            var model = service.GetSettingsIndexViewModel(104183931);
+            return View(model);
         }
     }
 }
