eedadb12b3cd14ed2c433b5be266fef099ff4b75
nakaji <nakaji126@gmail.com>
2016-07-09 04:05:28 +0900

    Add:ワーカーサービスを追加

---
+++ Nuts.Web/WorkerService/SettingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nuts.Web.ViewModels;
+
+namespace Nuts.Web.WorkerService
+{
+    public class SettingService
+    {
+        public SettingsIndexViewModel GetSettingsIndexViewModel(long userId)
+        {
+            return null;
+        }
+    }
+}
