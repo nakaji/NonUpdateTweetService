d438572b71ac758d438e48284d58d389727f6e25
nakaji <nakaji126@gmail.com>
2016-07-08 09:00:16 +0900

    UserからSettingも持ってくるテストを追加

--- Nuts.Entity.Test/AppDbContextTest.cs
+++ Nuts.Entity.Test/AppDbContextTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nuts.Entity.Migrations;
 
@@ -30,6 +32,31 @@ namespace Nuts.Entity.Test
 
             // Assert
             var result = db.Users.ToListAsync().Result;
+
+        }
+
+        [TestMethod]
+        public void ユーザー取得時に設定も一緒に持ってくる()
+        {
+            // Arrange
+            Database.SetInitializer(new DropCreateDatabaseAlways<AppDbContext>());
+            var db = new AppDbContext();
+
+            db.Users.Add(new User()
+            {
+                UserId = 100,
+                Settings = new List<Setting>()
+                {
+                    new Setting() {RssUrl = "http://example.com/rss"}
+                }
+            });
+            db.SaveChanges();
+
+            // Act
+            var user = db.Users.Include("Settings").FirstOrDefault(x => x.UserId == 100);
+
+            // Assert
+            Assert.AreEqual("http://example.com/rss", user.Settings[0].RssUrl);
 
         }
     }
